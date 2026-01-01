908939f3333b394cb56b4bba2b76261c127b55bf
Abhijithmns <abhijithmns07@gmail.com>
2026-01-01 22:19:52 +0530

    update autostart

--- autostart.sh
+++ autostart.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
+export PATH="$HOME/.local/bin:/usr/local/bin:/usr/bin:/bin"
+wal -R
+picom &
+setxkbmap -option caps:escape_shifted_capslock &
+# restore pywal AFTER X is ready
+(
+    sleep 2
+    xrdb -merge "$HOME/.cache/wal/colors.Xresources"
+    dwmblocks &
+) &
 
-dwmblocks &
